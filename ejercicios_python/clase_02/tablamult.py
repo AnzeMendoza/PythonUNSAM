bc2e8682dc69d212e1115df0634fc4f01f822551
anze.mendoza <anze.mendoza@gmail.om>
2020-08-10 16:12:43 -0300

    se corrigio el ejercicio de generar tabla de multiplicar

--- ejercicios_python/clase_02/tablamult.py
+++ ejercicios_python/clase_02/tablamult.py
@@ -3,12 +3,11 @@ def tabla_multiplicar():
     cuenta = 0
     for i in range(0,10):
         print(f'{cuenta}\t',end='')
-        for j in range(0,10):
+        for j in range(0,9):
             cuenta += i
             print(f'{cuenta}\t',end='')
-        print('')
+        print('') # genera el salto de linea.
         cuenta = 0
-
 
 def main():
     tabla_multiplicar()
