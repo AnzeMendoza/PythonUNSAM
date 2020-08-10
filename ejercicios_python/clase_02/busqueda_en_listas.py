b1593e320789052ef45a1e6aec9035cdd77a5585
anze.mendoza <anze.mendoza@gmail.om>
2020-08-10 16:22:54 -0300

    se modifico lo que imprime la funcion de busqueda

--- ejercicios_python/clase_02/busqueda_en_listas.py
+++ ejercicios_python/clase_02/busqueda_en_listas.py
@@ -5,7 +5,6 @@ def busqueda_u_elemento(lista, item):
             indice = i
     return indice
 
-
 def buscar_n_elemento(lista, item):
     contador = 0
     for i in range(0, len(lista)):
@@ -13,28 +12,22 @@ def buscar_n_elemento(lista, item):
             contador += 1
     return contador
 
-
 def maximo(lista = None):
 
     try:
         if lista == None or len(lista) == 0 :
             raise RuntimeError
+
         maximo = lista[0]
         for i in range(1, len(lista)):
             if maximo < lista[i]:
                 maximo = lista[i]
         return maximo
     except RuntimeError:
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>> Lista vacia o inexistente")
-    
-
-    for i in range(1,len(lista)):
-        if maximo < lista[i]:
-            maximo = lista[i]
-    return maximo
+        return "Lista esta vacia o es inexistente"
 
 def main():
-    print(maximo())
+    print(maximo([]))
 
 if __name__ == "__main__":
     main()
